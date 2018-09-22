3c0a48d1800a00ca7212812a38833c1fdd379566
DevBorisElkin <ea.foreverrr@gmail.com>
2018-09-22 20:49:19 +0300

    minor change

--- app/src/main/java/com/example/eafor/clientserverapp/MainActivity.java
+++ app/src/main/java/com/example/eafor/clientserverapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.eafor.clientserverapp;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;

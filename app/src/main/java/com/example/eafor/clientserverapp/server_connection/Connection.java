1a7f5648cb4623e8473f11986e291a0b5f39a4c9
DevBorisElkin <ea.foreverrr@gmail.com>
2018-09-24 08:30:52 +0300

    Beginning for connection part

---
+++ app/src/main/java/com/example/eafor/clientserverapp/server_connection/Connection.java
@@ -0,0 +1,11 @@
+package com.example.eafor.clientserverapp.server_connection;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Connection implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ app/src/main/java/com/example/eafor/clientserverapp/server_connection/Initializable.java
@@ -0,0 +1,10 @@
+package com.example.eafor.clientserverapp.server_connection;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public interface Initializable {
+
+    public void initialize(URL location, ResourceBundle resources);
+}
